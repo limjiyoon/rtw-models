1e18001c1868eb9fd0d2c99b8fc9ce1876ab2ba9
limjiyoon <luinpark@gmail.com>
2025-01-27 08:05:40 +0900

    CHORE: check src.shape at generate_mask function with constant

--- src/rtw_models/transformer/generate_mask.py
+++ src/rtw_models/transformer/generate_mask.py
@@ -6,7 +6,8 @@ import torch
 def key_pad_mask(src: torch.Tensor, pad_token: int = 0) -> torch.Tensor:
     """Generate key padding mask for the input sequence."""
     # src: (batch_size, seq_len)
-    assert len(src.shape) == 2, f"Input shape should be (batch_size, seq_len) but got {src.shape}"
+    src_rank = 2
+    assert len(src.shape) == src_rank, f"Input shape should be (batch_size, seq_len) but got {src.shape}"
 
     # mask: (batch_size, 1, seq_len)
     return (src != pad_token).unsqueeze(-2)
@@ -22,7 +23,8 @@ def subsequent_mask(size: int, pad_token: int = 0) -> torch.Tensor:
 def make_tgt_mask(src: torch.Tensor, pad_token: int = 0) -> torch.Tensor:
     """Generate mask for the target sequence."""
     # src: (batch_size, seq_len)
-    assert len(src.shape) == 2, f"Input shape should be (batch_size, seq_len) but got {src.shape}"
+    src_rank = 2
+    assert len(src.shape) == src_rank, f"Input shape should be (batch_size, seq_len) but got {src.shape}"
 
     # mask: (batch_size, seq_len, seq_len)
     return (key_pad_mask(src, pad_token) & subsequent_mask(src.size(1), pad_token=pad_token)).to(src.device)
