c9957ed697e6709aafc8b4e90a05d7a601bac0fc
limjiyoon <luinpark@gmail.com>
2025-01-28 08:22:52 +0900

    FEATURE: add RoPE

--- src/rtw_models/commons/positional_encoding.py
+++ src/rtw_models/commons/positional_encoding.py
@@ -3,6 +3,7 @@
 import math
 
 import torch
+from loguru import logger
 from torch import nn
 
 
@@ -38,3 +39,56 @@ class SinusoidalPositionalEncoding(nn.Module):
         pe_val = self.pe[:, : x.size(1)]
         pe_val.requires_grad = False
         return self.dropout(x + pe_val)
+
+
+class RotaryEmbedding(nn.Module):
+    """Rotary Positional Embeddings.
+
+    Reference:
+        - https://arxiv.org/abs/2104.09864
+    """
+
+    def __init__(self, d_model: int, dropout: float, max_len: int, device: str | torch.device = "cpu"):
+        super().__init__()
+        assert d_model % 2 == 0, f"d_model should be even, but got {d_model}"
+        assert 0.0 <= dropout <= 1.0, f"Dropout should be in range [0, 1], got {dropout}"
+        self.device = torch.device(device)
+
+        theta = 10000
+        freqs = 1.0 / (theta ** (torch.arange(0, d_model, 2, device=device).float()[: (d_model // 2)] / d_model))
+        timesteps = torch.arange(max_len, device=device).float()
+        angles = torch.outer(timesteps, freqs).float()  # shape: max_len x (d_model // 2)
+
+        # cos.shape, sin.shape: (1, 1, max_len, d_model // 2)
+        cos = torch.cos(angles)[None, None, :, :]
+        sin = torch.sin(angles)[None, None, :, :]
+
+        self.register_buffer("cos", cos)
+        self.register_buffer("sin", sin)
+
+    @torch.no_grad()
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply rotary positional embeddings to the input tensor.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (batch_size, seq_len, n_heads, d_model)
+
+        Returns:
+            torch.Tensor: Output tensor with rotary positional embeddings.
+        """
+        # x.shape: (batch_size, seq_len, n_heads d_model)
+        assert (
+            x.shape[-2] == self.cos.shape[-2]
+        ), f"Input sequence length {x.shape[-2]} and max_len {self.cos.shape[-2]} should be equal"
+        assert (
+            x.shape[-1] == self.cos.shape[-1] * 2
+        ), f"Input d_model {x.shape[-1]} should be twice the d_model of cos {self.cos.shape[-1]}"
+        assert x.device == self.device, f"Layer device {self.device} and Input device {x.device} should be equivalent"
+
+        # x_real.shape: (batch_size, seq_len, n_heads, d_model // 2)
+        x_real, x_image = x.reshape(x.shape[:-1] + (-1, 2)).unbind(-1)
+
+        out_real = x_real * self.cos - x_image * self.sin
+        out_image = x_real * self.sin + x_image * self.cos
+        out = torch.stack((out_real, out_image), dim=-1).flatten(-2)
+        return out.type_as(x)
--- tests/commons/test_positional_encodings.py
+++ tests/commons/test_positional_encodings.py
@@ -1,6 +1,6 @@
 import torch
 
-from rtw_models.commons.positional_encoding import SinusoidalPositionalEncoding
+from rtw_models.commons.positional_encoding import RotaryEmbedding, SinusoidalPositionalEncoding
 
 
 class TestPositionalEncodings:
@@ -11,6 +11,19 @@ class TestPositionalEncodings:
 
         encoding = SinusoidalPositionalEncoding(d_model=d_model, dropout=dropout, max_len=seq_len, device=device)
         x = torch.ones((batch_size, seq_len, d_model))
+        output = encoding(x)
+
+        assert output.shape == x.shape, "Output shape should be same as input"
+        assert output.device == x.device, "Output device should be same as input device"
+        assert torch.allclose(output[0], output[1]), "Output should be same for all batches"
+
+    def test_rotary_embedding_is_executable(self):
+        batch_size, n_heads, seq_len, d_model = 2, 2, 3, 4
+        dropout = 0.0
+        device = "cpu"
+
+        encoding = RotaryEmbedding(d_model=d_model, dropout=dropout, max_len=seq_len, device=device)
+        x = torch.ones((batch_size, n_heads, seq_len, d_model))
         output = encoding(x)
 
         assert output.shape == x.shape, "Output shape should be same as input"
