3adf14334935bfd57b6878fb405ee6f49573a4fd
limjiyoon <luinpark@gmail.com>
2025-01-28 09:22:37 +0900

    FEATURE: add grouped query attention and its test code

--- src/rtw_models/commons/multi_head_attention.py
+++ src/rtw_models/commons/multi_head_attention.py
@@ -7,6 +7,26 @@ from torch import nn
 from torch.nn import functional as F
 
 
+def repeat_head(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    """Repeat the tensor along the head dimension.
+
+    Args:
+        x (torch.Tensor): Input tensor to repeat.
+        n_rep (int): Number of times to repeat the tensor.
+
+    Returns:
+        torch.Tensor: Repeated tensor.
+    """
+    batch_size, n_kv_heads, seq_len, head_dim = x.shape
+    if n_rep == 1:
+        return x
+    return (
+        x[:, :, None, :, :]
+        .expand(batch_size, n_kv_heads, n_rep, seq_len, head_dim)
+        .reshape(batch_size, n_kv_heads * n_rep, seq_len, head_dim)
+    )
+
+
 def attention(
     query: torch.Tensor,
     key: torch.Tensor,
@@ -14,6 +34,7 @@ def attention(
     mask: torch.Tensor | None,
     dropout: nn.Module,
     *,
+    pe: nn.Module | None = None,
     mask_fill_value: float = -1e9,
 ) -> tuple[torch.Tensor, torch.Tensor]:
     """Compute the attention weights and the attended values.
@@ -24,6 +45,7 @@ def attention(
         value (torch.Tensor): Value tensor of shape (batch_size, n_heads, seq_len, d_key).
         dropout (nn.Module): Dropout module to apply to the attention weights.
         mask (torch.Tensor | None, optional): Mask tensor of shape (batch_size, seq_len, seq_len). Defaults to None.
+        pe (nn.Module | None, optional, keyword only): Positional encoding module. Defaults to None.
         mask_fill_value (float, optional, keyword only): Value to replace the masked values with. Defaults to -1e9.
 
 
@@ -33,6 +55,10 @@ def attention(
             - The attention weights tensor of shape (batch_size, n_heads, seq_len, seq_len).
     """
     assert isinstance(dropout, nn.Dropout | nn.Identity), "Dropout should be an instance of nn.Dropout or nn.Identity."
+    if pe is not None:
+        query = pe(query)
+        key = pe(key)
+
     # query: (batch_size, n_heads, seq_len, d_key)
     batch_size, n_heads, seq_len, d_model = query.size()
 
@@ -90,9 +116,83 @@ class MultiHeadAttention(nn.Module):
         q = self.q_layer(query).view(batch_size, seq_len, self.n_heads, self.d_key).transpose(1, 2)
         k = self.k_layer(key).view(batch_size, seq_len, self.n_heads, self.d_key).transpose(1, 2)
         v = self.v_layer(value).view(batch_size, seq_len, self.n_heads, self.d_key).transpose(1, 2)
+
         attn_value_per_head, _ = attention(q, k, v, dropout=self.dropout, mask=mask)
 
         attn_value = attn_value_per_head.transpose(1, 2).contiguous().view(batch_size, seq_len, d_model)
 
         # Output: (batch_size, seq_len, d_model)
         return self.o_layer(attn_value)
+
+
+class GroupedQueryAttention(nn.Module):
+    """Grouped Query Attention Layer.
+
+    Reference:
+        - https://arxiv.org/abs/2305.13245
+    """
+
+    def __init__(
+        self,
+        n_heads: int,
+        n_kv_heads: int,
+        d_model: int,
+        dropout: float,
+        pe: nn.Module | None,
+        device: str | torch.device,
+    ):
+        super().__init__()
+        assert d_model % n_heads == 0, f"d_model ({d_model}) should be divisible by n_heads ({n_heads})."
+        assert n_heads % n_kv_heads == 0, f"n_heads ({n_heads}) should be divisible by n_kv_heads ({n_kv_heads})."
+        assert 0.0 <= dropout <= 1.0, "Dropout should be in range [0, 1]"
+
+        self.pe = pe
+        self.device = torch.device(device)
+        if pe is not None:
+            assert (
+                pe.device == self.device
+            ), f"Layer device {self.device} and PE device {pe.device} should be equivalent"
+
+        self.d_head = d_model // n_heads
+        self.n_heads = n_heads
+        self.n_kv_heads = n_kv_heads
+        self.n_rep = n_heads // n_kv_heads
+
+        self.q_layer = nn.Linear(d_model, d_model, bias=False, device=self.device)
+        self.k_layer = nn.Linear(d_model, self.n_kv_heads * self.d_head, bias=False, device=self.device)
+        self.v_layer = nn.Linear(d_model, self.n_kv_heads * self.d_head, bias=False, device=self.device)
+        self.o_layer = nn.Linear(d_model, d_model, bias=False, device=self.device)
+
+        self.dropout = nn.Dropout(dropout) if dropout != 0 else nn.Identity()
+
+    def forward(
+        self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor, mask: torch.Tensor | None = None
+    ) -> torch.Tensor:
+        """Forward pass for the multi-head attention layer.
+
+        Args:
+            query (torch.Tensor): Query tensor of shape (batch_size, seq_len, d_model).
+            key (torch.Tensor): Key tensor of shape (batch_size, seq_len, d_model).
+            value (torch.Tensor): Value tensor of shape (batch_size, seq_len, d_model).
+            mask (torch.Tensor | None, optional): Mask tensor of shape (batch_size, seq_len, seq_len). Defaults to None.
+
+        Returns:
+            torch.Tensor: Output tensor after applying multi-head attention.
+        """
+        batch_size, seq_len, d_model = query.size()
+
+        # d_model = n_heads * d_key
+        # q : (batch_size, n_heads, seq_len, d_key)
+        # k, v : (batch_size, n_kv_heads, seq_len, d_head)
+        q = self.q_layer(query).view(batch_size, seq_len, self.n_heads, self.d_head).transpose(1, 2)
+        k = self.k_layer(key).view(batch_size, seq_len, self.n_kv_heads, self.d_head).transpose(1, 2)
+        v = self.v_layer(value).view(batch_size, seq_len, self.n_kv_heads, self.d_head).transpose(1, 2)
+
+        k = repeat_head(k, self.n_rep)
+        v = repeat_head(v, self.n_rep)
+        attn_value_per_head, _ = attention(q, k, v, mask=mask, dropout=self.dropout, pe=self.pe)
+
+        attn_value = attn_value_per_head.transpose(1, 2).contiguous().view(batch_size, seq_len, d_model)
+
+        # Output: (batch_size, seq_len, d_model)
+        return self.dropout(self.o_layer(attn_value))
--- tests/commons/test_multi_head_attention.py
+++ tests/commons/test_multi_head_attention.py
@@ -1,10 +1,32 @@
 import torch
 from torch import nn
 
-from rtw_models.commons.multi_head_attention import MultiHeadAttention, attention
+from rtw_models.commons.multi_head_attention import MultiHeadAttention, attention, GroupedQueryAttention, repeat_head
+from rtw_models.commons.positional_encoding import RotaryEmbedding
 
 
 class TestMultiHeadAttention:
+    def test_repeat_head_returns_correct_shape(self):
+        """Check if the repeat_head function returns the correct shape.
+
+        Checklist:
+            - Check the shape of the output tensor.
+        """
+        batch_size, n_rep, seq_len, d_model = 2, 3, 4, 4
+        x = torch.ones((batch_size, 1, seq_len, d_model))
+
+        output = repeat_head(x, n_rep)
+
+        assert output.shape == (
+            batch_size,
+            n_rep,
+            seq_len,
+            d_model,
+        ), "Output shape should be (batch_size, n_heads, seq_len, d_model)"
+        assert torch.allclose(output[:, 0], output[:, 1]) and torch.allclose(
+            output[:, 1], output[:2]
+        ), "Output should be same for all heads"
+
     def test_attention_executable(self):
         """Check if the attention function is executable.
 
@@ -25,6 +47,41 @@ class TestMultiHeadAttention:
         query, key, value = x, x, x
         mask = torch.ones((batch_size, 1, seq_len, seq_len), device=device)
         attn_values, attn_weights = attention(query=query, key=key, value=value, mask=mask, dropout=dropout)
+
+        assert (
+            attn_values.shape == x.shape
+        ), f"Attention values shape ({attn_values.shape}) should be same as value shape {value.shape}"
+        assert (
+            attn_values.device == device
+        ), f"Attention values device {attn_values.device} should be same as input device {device}"
+        assert (
+            attn_weights.shape == (batch_size, n_heads, seq_len, seq_len)
+        ), f"Attention weights shape should be ({batch_size}, {n_heads}, {seq_len}, {seq_len}) but got {attn_weights.shape}"
+        assert (
+            attn_weights.device == device
+        ), f"Attention weights device {attn_weights.device} should be same as input device {device}"
+
+    def test_attention_with_pe_executable(self):
+        """Check if the attention function is executable with positional encoding.
+
+        Checklist:
+            - Check the shape of the attention_values/attention_weights tensors.
+            - Check the device of the attention_values/attention_weights tensors.
+        """
+        # d_model should be even
+        batch_size, n_heads, seq_len, d_model = 2, 3, 4, 4
+        dropout = nn.Identity()
+        device = torch.device("cpu")
+
+        x = (
+            torch.arange(batch_size * n_heads * seq_len * d_model, device=device)
+            .float()
+            .reshape(batch_size, n_heads, seq_len, d_model)
+        )
+        query, key, value = x, x, x
+        mask = torch.ones((batch_size, 1, seq_len, seq_len), device=device)
+        pe = RotaryEmbedding(d_model=d_model, dropout=0.0, max_len=seq_len, device=device)
+        attn_values, attn_weights = attention(query=query, key=key, value=value, mask=mask, dropout=dropout, pe=pe)
 
         assert (
             attn_values.shape == x.shape
@@ -76,6 +133,30 @@ class TestMultiHeadAttention:
         device = "cpu"
 
         mha = MultiHeadAttention(n_heads=n_heads, d_model=d_model, dropout=dropout, device=device)
+        x = torch.ones((batch_size, seq_len, d_model))
+        output = mha(query=x, key=x, value=x, mask=None)
+
+        assert output.shape == x.shape, "Output shape should be same as input"
+        assert output.device == x.device, "Output device should be same as input device"
+        assert torch.allclose(output[0], output[1]), "Output should be same for all batches"
+
+    def test_grouped_query_attention_is_executable(self):
+        """Check if the GroupedQueryAttention layer is executable.
+
+        Checklist:
+            - Check the shape of the output tensor.
+            - Check the device of the output tensor.
+        """
+        # d_model should be divisible by n_heads
+        batch_size, n_heads, n_kv_heads, seq_len, d_model = 2, 4, 2, 4, 8
+        head_dim = d_model // n_heads
+        dropout = 0.0
+        device = "cpu"
+        pe = RotaryEmbedding(d_model=head_dim, dropout=0.0, max_len=seq_len, device=device)
+
+        mha = GroupedQueryAttention(
+            n_heads=n_heads, n_kv_heads=n_kv_heads, d_model=d_model, dropout=dropout, pe=pe, device=device
+        )
         x = torch.ones((batch_size, seq_len, d_model))
         output = mha(query=x, key=x, value=x, mask=None)
 
